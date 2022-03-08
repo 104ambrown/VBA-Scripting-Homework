11c26460a43c72ddf0368644256664ffb1a8dd15
Zas Mama <104ambrown@gmail.com>
2022-03-08 18:00:16 -0500

    finished the rest of the script including printing the results to the analysis table and formatting things.

--- Sub MultipleYearStockAnalysis().vb
+++ Sub MultipleYearStockAnalysis().vb
@@ -33,18 +33,19 @@ Sub MultipleYearStockAnalysis()
     dim max_volume as double
     dim tickerPMin as string
     dim tickerPMax as string
+    dim tickerMaxVol as string
     
     ' Creating Analysis Table headers
-    ws.Range("I1").Value = "Ticker"
-    ws.Range("J1").Value = "Yearly Change"
-    ws.Range("K1").Value = "% Change"
-    ws.Range("L1").Value = "Total Stock Volume"
+    ws.Range("I1").value = "Ticker"
+    ws.Range("J1").value = "Yearly Change"
+    ws.Range("K1").value = "% Change"
+    ws.Range("L1").value = "Total Stock Volume"
     ' Creating Analysis Table and labels for High/Low Stats
-    ws.Range("O2").Value = "Greatest % Increase"
-    ws.Range("O3").Value = "Greatest % Decrease"
-    ws.Range("O4").Value = "Greatest Total Volume"
-    ws.Range("P1").Value = "Ticker"
-    ws.Range("Q1").Value = "Value"
+    ws.Range("O2").value = "Greatest % Increase"
+    ws.Range("O3").value = "Greatest % Decrease"
+    ws.Range("O4").value = "Greatest Total Volume"
+    ws.Range("P1").value = "Ticker"
+    ws.Range("Q1").value = "Value"
     
     'Instantiating data for the first ticker
     open = range("C2")
@@ -53,41 +54,72 @@ Sub MultipleYearStockAnalysis()
     total_stockVol = -1
 
     ' Iterating through the data
-    row_index = 2
+    rowIndex = 2
     total_stockVol = 0
     last_row = cells(rows.count, 1).end(x1up).row
     
     for i = 2 to last_row
-        ticker = cells(i, 1).Value
-        newTicker = cells(i + 1, 1).Value
-        total_stockVol = cells(i, 7).Value
+        ticker = cells(i, 1).value
+        newTicker = cells(i + 1, 1).value
+        total_stockVol = cells(i, 7).value
         total_stockVol = total_stockVol + volume
 
         ' Adding new ticker values and setting volum back to 0 when a new ticker is found
         if (ticker <> newTicker) then
             close = cells(i, 6).value
-            cells(row_index, 9).value = ticker
-            cells(row_index, 10).value = close - open
-            cells(row_index, 10).numberformat = "0.0000000000"
+            cells(rowIndex, 9).value = ticker
+            cells(rowIndex, 10).value = close - open
+            cells(rowIndex, 10).numberformat = "0.0000000000"
 
-            if (cells(row_index, 10).value >=0) Then
-                cells(row_index, 10).interior.colorindex = 10
+            if (cells(rowIndex, 10).value >=0) Then
+                cells(rowIndex, 10).interior.colorindex = 10
             else
-                cells(row_index, 10).interior.colorindex = 9
+                cells(rowIndex, 10).interior.colorindex = 9
             end if
 
-            cells(row_index, 11).numberformat = "0.00%"
+            cells(rowIndex, 11).numberformat = "0.00%"
             If (open <> 0) then
-                cellscells(row_index, 11).value = (close - open) / open
+                cellscells(rowIndex, 11).value = (close - open) / open
             else
-                cells_row_index, 11).value = 0
+                cells(rowIndex, 11).value = 0
             end if
 
-            cells(row_index, 12).value = total_stockVol
+            cells(rowIndex, 12).value = total_stockVol
 
-            cells(row_index, 12).value = total_stockVol
+            ' Updating pecent minimum and maximum values and total stock volume values
+            if (cells(rowIndex, 11).value < percent_max) then
+                percent_maz = cells(rowIndex, 11).value
+                tickerPMax = ticker
+            else if (cells(rowIndex, 11).value < percent_min) then
+                tickerPMin = ticker
+            end if 
+            if (total_stockVol > max_volume) then
+                max-volume = total_stockVol
+                tickerMaxVol = ticker
+            end if
 
+            rowIndex = rowIndex + 1
+            open = cells(i + 1, 3)
+            total_stockVol = 0
 
-    
+        end if 
+
+    next i
+
+    ' Printing all of the results
+    range("O2").value = "Greatest % Increase"
+    range("O3").value = "Greatest % Decrease"
+    range("O4").value = "Greatest Total Volume"
+    range("P1").value = "Ticker"
+    range("P2").value = tickerPMax
+    range("P3").value = tickerPMin
+    range("P4").value = tickerMaxVol
+    range("Q1").value = "Value"
+    range("Q2").numberformat = "0.00%"
+    range("Q2").value = percent_max
+    range("Q3").numberformat = "0.00%"
+    range("Q3").value = percent_min
+    range("Q4").value = max_volume
+
 End Sub
 
