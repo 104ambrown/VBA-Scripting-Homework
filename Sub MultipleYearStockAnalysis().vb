816f8894de97d77214194d37f903b9dcac91d053
Zas Mama <104ambrown@gmail.com>
2022-03-08 17:06:33 -0500

    Initialized for loop to iterate through worksheets. Declared Variables.

--- Sub MultipleYearStockAnalysis().vb
+++ Sub MultipleYearStockAnalysis().vb
@@ -1,12 +1,34 @@
+Sub loopingWorksheets ()
+
+    ' Declaring and initializing worksheets
+    dim WSCount as integer
+    WSCount = ActiveWorkbook.Worksheets.count
+    dim activeWS as Worksheet
+
+    ' For loop to loop through worksheets
+    For i = 1 to WSCount
+        ActiveWorkbook.Worksheets(i).Activate 
+        Call MultipleYearStockAnalysis
+    Next i
+End Sub
+
 Sub MultipleYearStockAnalysis()
 
     ' Declaring worksheet variable
-    Dim ws As Worksheet
-    
+    dim ws As Worksheet
+    ' Declaring variables for columns in the worksheet
+    dim ticker as string
+    dim open as double 
+    dim close as double
+    dim yearly_change as double
+    dim total_stockVol as double
+    dim percent_change as double
+    dim starting_point as integer
+
     ' Looping through the worksheets
     For Each ws In Worksheets
     
-    ' Creating Analysis Tables for All Tickers
+    ' Creating Analysis Table headers
     ws.Range("I1").Value = "Ticker"
     ws.Range("J1").Value = "Yearly Change"
     ws.Range("K1").Value = "% Change"
@@ -18,8 +40,9 @@ Sub MultipleYearStockAnalysis()
     ws.Range("O1").Value = "Ticker"
     ws.Range("P1").Value = "Value"
     
-    ' Declaring variables
-    
+    ' Declaring row variable
+
+    last_row = cells(rows.count)
     
     
 End Sub
